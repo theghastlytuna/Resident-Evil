f66682ede3c296631c20650703f312dddaa98e5c
Trevor Long <trevor.long@ontariotechu.net>
2020-12-04 23:24:25 -0500

    'sProperBranch: Auto stash before merge of "Trevor'sProperBranch" and "origin/JordanWorkBranch"

--- SpriteLib3.0-v2.0/ResidentEvil.cpp
+++ SpriteLib3.0-v2.0/ResidentEvil.cpp
@@ -94,6 +94,45 @@ void ResidentEvil::InitScene(float windowWidth, float windowHeight)
 
 	}
 
+	//enemy
+	{
+		auto entity = ECS::CreateEntity();
+
+		//Add components
+		ECS::AttachComponent<Sprite>(entity);
+		ECS::AttachComponent<Transform>(entity);
+		ECS::AttachComponent<PhysicsBody>(entity);
+		ECS::AttachComponent<Health>(entity);
+
+		//set components
+		std::string fileName = "zombie_top_down.png";
+		ECS::GetComponent<Sprite>(entity).LoadSprite(fileName, 50, 50);
+		ECS::GetComponent<Sprite>(entity).SetTransparency(1.f);
+		ECS::GetComponent<Transform>(entity).SetPosition(vec3(0.f, 30.f, 2.f));
+		ECS::GetComponent<Health>(entity).health = 50;
+
+		auto& tempSpr = ECS::GetComponent<Sprite>(entity);
+		auto& tempPhsBody = ECS::GetComponent<PhysicsBody>(entity);
+
+		float shrinkX = 30.f;
+
+		b2Body* tempBody;
+		b2BodyDef tempDef;
+		tempDef.type = b2_staticBody;
+		tempDef.position.Set(float32(60.f), float32(0.f));
+
+		tempBody = m_physicsWorld->CreateBody(&tempDef);
+
+		tempPhsBody = PhysicsBody(entity, tempBody, float(tempSpr.GetWidth() - shrinkX), vec2(0.f, 0.f), false, 
+				ENEMY, PLAYER | OBJECTS | GROUND | ENVIRONMENT, 0.5f, 1.2f); //circle body
+
+		tempPhsBody.SetRotationAngleDeg(0.f);
+		tempPhsBody.SetColor(vec4(1.f, 0.f, 1.f, 0.3f));
+
+	}
+
+
+
 	ECS::GetComponent<HorizontalScroll>(MainEntities::MainCamera()).SetFocus(&ECS::GetComponent<Transform>(MainEntities::MainPlayer()));
 	ECS::GetComponent<VerticalScroll>(MainEntities::MainCamera()).SetFocus(&ECS::GetComponent<Transform>(MainEntities::MainPlayer()));
 }
@@ -167,9 +206,56 @@ void ResidentEvil::KeyboardHold()
 
 void ResidentEvil::KeyboardDown()
 {
+	auto& player = ECS::GetComponent<PhysicsBody>(MainEntities::MainPlayer());
+	//auto& bulletEntity = ECS::GetComponent<PhysicsBody>(bulletPhyBody);
+
 	if (Input::GetKeyDown(Key::T))
 	{
 		PhysicsBody::SetDraw(!PhysicsBody::GetDraw());
+	}
+
+	if (Input::GetKeyDown(Key::W) && Input::GetKey(Key::Space))
+	{
+		//m_physicsWorld->CreateBody(bulletDef)
+		//bulletEntity.SetPosition(player.GetPosition().x, player.GetPosition().y);
+
+			//Create Bullet Entity
+		{
+			auto bulletEntity = ECS::CreateEntity();
+			//auto& player = ECS::GetComponent<PhysicsBody>(MainEntities::MainPlayer());
+
+			//Adding Components
+			ECS::AttachComponent<Sprite>(bulletEntity);
+			ECS::AttachComponent<Transform>(bulletEntity);
+			ECS::AttachComponent<PhysicsBody>(bulletEntity);
+
+
+			std::string fileName = "whiteBall.png";
+			ECS::GetComponent<Sprite>(bulletEntity).LoadSprite(fileName, 5, 5);
+			ECS::GetComponent<Sprite>(bulletEntity).SetTransparency(1.f);
+			ECS::GetComponent<Transform>(bulletEntity).SetPosition(vec3(player.GetPosition().x, player.GetPosition().y, 10.f));
+
+			auto& bulletSpr = ECS::GetComponent<Sprite>(bulletEntity);
+			auto& bulletPhsBody = ECS::GetComponent<PhysicsBody>(bulletEntity);
+
+			float shrinkX = 5.f;
+
+			b2Body* bulletBody;
+			b2BodyDef bulletDef;
+			bulletDef.type = b2_dynamicBody;
+			bulletDef.position.Set(player.GetPosition().x, player.GetPosition().y);
+
+			bulletBody = m_physicsWorld->CreateBody(&bulletDef);
+
+			bulletPhsBody = PhysicsBody(bulletEntity, bulletBody, float(bulletSpr.GetWidth() - shrinkX), vec2(0.f, 0.f), false,
+				ENEMY , OBJECTS | PICKUP | TRIGGER | GROUND | ENVIRONMENT, 0.35f, 1.2f); //circle body
+
+			bulletBody->SetFixedRotation(true);
+			bulletPhsBody.SetRotationAngleDeg(0.f);
+			bulletPhsBody.SetColor(vec4(0.f, 1.f, 6.f, 0.3f));
+		}
+		
+		
 	}
 
 	if (Input::GetKeyDown(Key::J))
